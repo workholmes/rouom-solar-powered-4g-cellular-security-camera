7a3d785e246f609ef16a213667aa3c33007a66f3
workholmes <workholmes@outlook.com>
2023-06-27 15:12:13 +0800

    description page
    description page

---
+++ static/js/kbr.js
@@ -0,0 +1,1 @@
+if(typeof console!="undefined"){console.log("Kickbooster is not installed but the Kickbooster script is still being loaded. Please read https://intercom.help/kickbooster/launching-a-referral-program/launching-a-referral-program-for-my-shopify-store/shopify-app-uninstall-instructions to fix.");}
